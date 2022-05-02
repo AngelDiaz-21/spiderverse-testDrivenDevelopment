1d01945b941f8079fa8ac9027411fcf66258d37a
Angel <angelSistemas21@gmail.com>
2022-05-02 02:04:59 -0500

    add: Class spiderman y atributos en el archivo spiderman.js

---
+++ app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+    constructor(nombre, edad, actorInterprete, numeroPelículasAparecio, estudioDeCine){
+        this.nombre = nombre
+        this.edad = edad
+        this.actorInterprete = actorInterprete
+        this.numeroPelículasAparecio = numeroPelículasAparecio
+        this.estudioDeCine = estudioDeCine
+    };
+};
+
+module.exports = Spiderman;
